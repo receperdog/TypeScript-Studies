6bffd622b92a770cd3539e08aec1dadd3148a122
Recep Tayyip Erdogan <66259609+rtayyiperdogan@users.noreply.github.com>
2022-02-06 00:22:27 +0300

    constructor added

--- 06 Interfaces/interface.ts
+++ 06 Interfaces/interface.ts
@@ -18,12 +18,15 @@ interface Vehicle {
 
 class Taxi implements Vehicle {
   currentLocation: location;
+  constructor(location: location) {
+    this.currentLocation = location;
+  }
   travelTo(location: location): void {
     console.log(`Taxi is going to ${location.x} , ${location.y}`);
   }
 }
 
-let sariTaxi = new Taxi();
+let sariTaxi = new Taxi({ x: 10, y: 11 });
 //sariTaxi.currentLocation.x = 172;
 //sariTaxi.currentLocation.y = 184;
-sariTaxi.travelTo({ x: 10, y: 11 });
+sariTaxi.travelTo({ x: 100, y: 111 });
