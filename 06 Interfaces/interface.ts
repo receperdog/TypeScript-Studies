9d6fffe042c1590e2d41139950aca8a0826f34c2
Recep Tayyip Erdogan <66259609+rtayyiperdogan@users.noreply.github.com>
2022-02-05 22:09:37 +0300

    interfaces

---
+++ 06 Interfaces/interface.ts
@@ -0,0 +1,31 @@
+interface location {
+  x: number;
+  y: number;
+}
+
+interface Passenger {
+  name: string;
+  address: string;
+}
+
+interface Vehicle {
+  currentLocation: location;
+  travelTo(location: location): void;
+  //getDistance(currentLocation, destinationLocation: Location);
+  //getPassengers(passenger: Passenger): void;
+  //removePassenger(passenger: Passenger): void;
+}
+
+class Taxi implements Vehicle {
+  currentLocation: location;
+  travelTo(location: location): void {
+    console.log(
+      `Taxi is going to ${location.x} , ${location.y} from ${this.currentLocation.x} , ${this.currentLocation.y} `
+    );
+  }
+}
+
+let sariTaxi = new Taxi();
+sariTaxi.currentLocation.x = 172;
+sariTaxi.currentLocation.y = 184;
+sariTaxi.travelTo({ x: 10, y: 11 });
