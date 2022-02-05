3de95d552108d8d944e472a5e4011f3c945f507b
Recep Tayyip Erdogan <66259609+rtayyiperdogan@users.noreply.github.com>
2022-02-06 00:48:24 +0300

    class and exception studies

--- 06 Interfaces/interface.js
+++ 06 Interfaces/interface.js
@@ -1,13 +1,27 @@
 var Taxi = /** @class */ (function () {
-  function Taxi() {}
-  Taxi.prototype.travelTo = function (location) {
-    console.log(
-      "Taxi is going to ".concat(location.x, " , ").concat(location.y)
-    );
-  };
-  return Taxi;
-})();
-var sariTaxi = new Taxi();
-//sariTaxi.currentLocation.x = 172;
-//sariTaxi.currentLocation.y = 184;
-sariTaxi.travelTo({ x: 10, y: 11 });
+    function Taxi(location, color) {
+        this.currentLocation = location;
+        this.color = color;
+    }
+    Taxi.prototype.setLocation = function (x, y) {
+        if (x < 0 || y < 0) {
+            throw new Error("Coordinates can not be less than 0");
+        }
+        else {
+            this.currentLocation.x = x;
+            this.currentLocation.y = y;
+        }
+    };
+    Taxi.prototype.getLocation = function () {
+        return "Taxi's current location is ".concat(this.currentLocation.x, " , ").concat(this.currentLocation.y, " ");
+    };
+    Taxi.prototype.toString = function () {
+        return "Car's color is ".concat(this.color, " and current location is ").concat(this.currentLocation.x, " , ").concat(this.currentLocation.y);
+    };
+    return Taxi;
+}());
+var sariTaxi = new Taxi({ x: 10, y: 11 }, "blue");
+//console.log(sariTaxi.toString());
+console.log(sariTaxi.getLocation() + "\n");
+sariTaxi.setLocation(-2, 20);
+console.log(sariTaxi.getLocation() + "\n");
--- 06 Interfaces/interface.ts
+++ 06 Interfaces/interface.ts
@@ -3,30 +3,50 @@ interface location {
   y: number;
 }
 
+class Taxi {
+  private color: string;
+  private currentLocation: location;
+
+  constructor(location: location, color: string) {
+    this.currentLocation = location;
+    this.color = color;
+  }
+
+  public setLocation(x: number, y: number): void {
+    if (x < 0 || y < 0) {
+      throw new Error("Coordinates can not be less than 0");
+    } else {
+      this.currentLocation.x = x;
+      this.currentLocation.y = y;
+    }
+  }
+
+  public getLocation(): string {
+    return `Taxi's current location is ${this.currentLocation.x} , ${this.currentLocation.y} `;
+  }
+
+  toString(): string {
+    return `Car's color is ${this.color} and current location is ${this.currentLocation.x} , ${this.currentLocation.y}`;
+  }
+}
+
+let sariTaxi = new Taxi({ x: 10, y: 11 }, "blue");
+//console.log(sariTaxi.toString());
+
+console.log(sariTaxi.getLocation() + "\n");
+sariTaxi.setLocation(-2, 20);
+console.log(sariTaxi.getLocation() + "\n");
+
+//console.log(sariTaxi.toString());
+
 interface Passenger {
   name: string;
   address: string;
 }
 
 interface Vehicle {
-  currentLocation: location;
-  travelTo(location: location): void;
+  //travelTo(location: location): void;
   //getDistance(currentLocation, destinationLocation: Location);
   //getPassengers(passenger: Passenger): void;
   //removePassenger(passenger: Passenger): void;
 }
-
-class Taxi implements Vehicle {
-  currentLocation: location;
-  constructor(location: location) {
-    this.currentLocation = location;
-  }
-  travelTo(location: location): void {
-    console.log(`Taxi is going to ${location.x} , ${location.y}`);
-  }
-}
-
-let sariTaxi = new Taxi({ x: 10, y: 11 });
-//sariTaxi.currentLocation.x = 172;
-//sariTaxi.currentLocation.y = 184;
-sariTaxi.travelTo({ x: 100, y: 111 });
